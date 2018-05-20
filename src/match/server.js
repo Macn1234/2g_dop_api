234283cf44ed7d98d050329c5e0455e8597a1947
root <root@node-1.c.dop-app-204402.internal>
2018-05-20 21:07:38 +0000

    Modificaciones api

--- src/match/server.js
+++ src/match/server.js
@@ -1,4 +1,4 @@
-export const url = process.env.MATCH_URL || 'match-ms';
+export const url = process.env.EMPAREJAMIENTO_URL || 'emparejamiento-ms';
 //export const url = process.env.MATCH_URL || '35.227.46.47';
-export const port = process.env.MATCH_PORT || '4003';
-export const entryPoint = process.env.MATCH_ENTRY || 'match';
+export const port = process.env.EMPAREJAMIENTO_PORT || '4004';
+export const entryPoint = process.env.EMPAREJAMIENTO_ENTRY || 'emparejamiento';
