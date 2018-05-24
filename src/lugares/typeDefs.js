cb76a8e216409b4e7ab64735a2c5a30920467585
Miguel Cortes <macortesn@unal.edu.co>
2018-05-23 21:58:16 -0500

    Modificacion API lugar

--- src/lugares/typeDefs.js
+++ src/lugares/typeDefs.js
@@ -1,6 +1,6 @@
 export const lugaresTypeDef = `
 type Lugar {
-    _id: String!
+    _id: Int!
     nombre: String!
     nivelPrecio: String!
     ubicacion: Point!
@@ -10,32 +10,35 @@ type Lugar {
     tipo: String
     ciudad: String
     pais: String
-
 }
-
 type Point
 {
     type: String!
     coordinates: [Float]!
     index: String!
 }
-
 input LugarInput {
-    code: Int!
-    name: String!
-    credits: Int!
+    nombre: String!
+    nivelPrecio: String!
+    ubicacion: Point!
+    direccion: String
+    horario: String
+    calificacion: String
+    tipo: String
+    ciudad: String
+    pais: String
 }
 `;
 
 export const lugaresQueries = `
-    alllugares: [Lugar]!  
-    lugarById(id: String!): Lugar! 
+    alllugares: [Lugar]!
+    lugarById(id: String!): Lugar!
 `;
 
- 
+
 
 export const lugaresMutations = `
-    createLugar(id: String): Lugar!
-    deleteLugar(id: String!): Lugar!
-    updateLugar(id: String!): Lugar!
+    createLugar(lugar: LugarInput): Lugar!
+    deleteLugar(id: Int!): Lugar!
+    updateLugar(id: Int!): Lugar!
 `;
